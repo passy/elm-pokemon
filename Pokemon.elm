d6eeadbceaa094a55f830c011d992600f5e0d885
Pascal Hartig <phartig@rdrei.net>
2015-11-17 22:06:36 +0100

    Initial commit

---
+++ Pokemon.elm
@@ -0,0 +1,41 @@
+module Pokemon where
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Signal exposing (Signal, Mailbox, Address)
+import String
+
+type alias Model = String
+
+initialModel : Model
+initialModel = "pikachu"
+
+main : Signal Html
+main =
+  Signal.map (view actions.address) model
+
+type Action = NoOp | UpdateField String
+
+actions : Signal.Mailbox Action
+actions = Signal.mailbox NoOp
+
+model : Signal Model
+model = Signal.foldp update initialModel actions.signal
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    NoOp -> model
+    UpdateField str -> String.toLower str
+
+view : Address Action -> Model -> Html
+view address model = div []
+  [ img [ src <| "http://img.pokemondb.net/artwork/" ++ model ++ ".jpg" ] []
+  , br [] []
+  , input
+    [ on "input" targetValue (Signal.message address << UpdateField)
+    , autofocus True
+    , value model
+    ] []
+  ]
